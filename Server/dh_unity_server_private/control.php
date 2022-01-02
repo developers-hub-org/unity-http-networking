7f2994a522b7884c1d9aa387673d005275436fac
Armin Ahmadi <75361058+dh-ceo@users.noreply.github.com>
2022-01-02 20:46:48 +0330

    Add files via upload

---
+++ Server/dh_unity_server_private/control.php
@@ -0,0 +1,9 @@
+<?php
+    define('SERVER_VERSION', '1.0');
+    define('FORCE_CLIENT_UPDATE_TO_SERVER_VERSION', false);
+    define('ALLOW_MULTIPLE_ONLINE_SESSIONS', false);
+    define('UNDER_MAINTENANCE', false);
+    define('MAX_USERS', 999999999);
+    define('MAX_ACCOUNTS_PER_IP', 10);
+    define('CONNECTION_CHECK_PERIOD', 5);
+?>
