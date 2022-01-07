caed1f47858619dbef907575417629dcb892699c
Armin Ahmadi <75361058+dh-ceo@users.noreply.github.com>
2022-01-07 22:42:36 +0330

    Update core_response.php

--- Server/dh_unity_server_private/core_response.php
+++ Server/dh_unity_server_private/core_response.php
@@ -234,7 +234,7 @@
 		$headers = "MIME-Version: 1.0" . "\r\n"; 
 		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
 		// Additional headers 
-		$headers .= 'From: '.$fromName.'<'.$from.'>' . "\r\n"; 
+		// $headers .= 'From: '.$fromName.'<'.$from.'>' . "\r\n"; 
 		// $headers .= 'Cc: welcome@example.com' . "\r\n"; 
 		// $headers .= 'Bcc: welcome2@example.com' . "\r\n"; 
 		return mail($to, $subject, $html, $headers);
