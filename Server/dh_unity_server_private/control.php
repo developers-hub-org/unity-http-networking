454ad20d21220c6541ecfcdc6b162f92f13d50f2
Armin Ahmadi <75361058+dh-ceo@users.noreply.github.com>
2022-01-07 21:17:24 +0330

    Update control.php

--- Server/dh_unity_server_private/control.php
+++ Server/dh_unity_server_private/control.php
@@ -1,4 +1,7 @@
 <?php
+    define('COMPANY_NAME', 'Developers Hub');
+    define('COMPANY_LOGO', 'https://upload.wikimedia.org/wikipedia/commons/c/c4/Unity_2021.svg');
+    define('COMPANY_EMAIL', 'demo@company.com');
     define('CLIENT_VERSION', '1.0');
     define('FORCE_CLIENTS_TO_UPDATE', false);
     define('ALLOW_MULTIPLE_ONLINE_SESSIONS', false);
@@ -7,5 +10,7 @@
     define('CONNECTION_CHECK_PERIOD', 5);
     define('MAX_ACCOUNTS_PER_IP', 10);
 	define('MAX_MESSAGE_PER_PACKAGE', 10);
-	define('MAX_RETURN_ACCOUNTS_PER_PAGE', 100);
+    define('MAX_RETURN_ACCOUNTS_PER_PAGE', 100);
+    define('PHONE_VERIFICATION_CODE_EXPIRE_TIME', 120);
+    define('EMAIL_VERIFICATION_CODE_EXPIRE_TIME', 600);
 ?>
