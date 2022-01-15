0547bdbb137f004717debe3ff70c7abac11965bd
Armin Ahmadi <75361058+armin-a94@users.noreply.github.com>
2022-01-15 17:32:45 +0330

    Delete Server directory

--- Server/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define('DB_NAME', 'name');
-	define('DB_USER', 'user');
-	define('DB_PASSWORD', 'pass');
-	define('DB_HOST', 'localhost');
-	define('AES_PASSWORD', 'abcdefghijklmnopqrstuvwxyz012345');
-	define('MD5_PASSWORD', 'abcdefg');
-?>
